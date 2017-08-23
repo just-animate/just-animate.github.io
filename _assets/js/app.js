b8bceac59069d9209ddec901c59fed382ff0d9f4
Joseph Rex <joerexko@gmail.com>
2017-08-23 04:05:17 -0500

    Add doc pages with content

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -86,92 +86,108 @@ ready(function() {
   'use strict';
 
   // Header animation
-  var t1 = just.timeline();
+  if(typeof just !== 'undefined'){
+    var t1 = just.timeline();
 
-  t1.fromTo(0, 600, {
-    targets: '#A',
-    easing: 'ease-in',
-    web: {
-      x: [-160, -160],
-      y: [-240, -10],
-      rotate: [270, 0],
-      scale: [.3, .3],
-      opacity: [0, 1],
-      transformOrigin: ['center center', 'center bottom']
-    }
-  });
+    t1.fromTo(0, 600, {
+      targets: '#A',
+      easing: 'ease-in',
+      web: {
+        x: [-160, -160],
+        y: [-240, -10],
+        rotate: [270, 0],
+        scale: [.3, .3],
+        opacity: [0, 1],
+        transformOrigin: ['center center', 'center bottom']
+      }
+    });
 
-  t1.fromTo(800, 1400, {
-    targets: '#A',
-    web: {
-      x: [-160, 0],
-      y: [-10, 0],
-      scale: [.4, 1],
-      transformOrigin: ['center bottom', 'center bottom']
-    }
-  });
+    t1.fromTo(800, 1400, {
+      targets: '#A',
+      web: {
+        x: [-160, 0],
+        y: [-10, 0],
+        scale: [.4, 1],
+        transformOrigin: ['center bottom', 'center bottom']
+      }
+    });
 
-  t1.fromTo(1000, 1400, {
-    targets: '#J',
-    web: {
-      x: [-20, 0],
-      opacity: [0, 1],
-      scale: [.9, 1],
-      transformOrigin: ['center bottom', 'center bottom']
-    }
-  });
+    t1.fromTo(1000, 1400, {
+      targets: '#J',
+      web: {
+        x: [-20, 0],
+        opacity: [0, 1],
+        scale: [.9, 1],
+        transformOrigin: ['center bottom', 'center bottom']
+      }
+    });
 
-  t1.add({
-    targets: '#logo-words path',
-    from: 900,
-    duration: 200,
-    stagger: 20,
-    web: {
-      opacity: [0, 1],
-      scaleY: [.6, 1.05, 1]
-    }
-  });
+    t1.add({
+      targets: '#logo-words path',
+      from: 900,
+      duration: 200,
+      stagger: 20,
+      web: {
+        opacity: [0, 1],
+        scaleY: [.6, 1.05, 1]
+      }
+    });
 
-  t1.play();
+    t1.play();
 
-  var t2 = just.timeline();
+    var t2 = just.timeline();
 
-  t2.set({
-    targets: ['.header__callouts .button', '#slogan'],
-    web: {
-      opacity: 0
-    }
-  });
+    t2.set({
+      targets: ['.header__callouts .button', '#slogan'],
+      web: {
+        opacity: 0
+      }
+    });
 
-  t2.add({
-    targets: ['.header__callouts .button', '#slogan'],
-    duration: 800,
-    stagger: 150,
-    web: {
-      opacity: [0, 1],
-      scale: [.3, 1.1, 1]
-    }
-  });
+    t2.add({
+      targets: ['.header__callouts .button', '#slogan'],
+      duration: 800,
+      stagger: 150,
+      web: {
+        opacity: [0, 1],
+        scale: [.3, 1.1, 1]
+      }
+    });
 
-  t2.pause();
+    t2.pause();
 
-  t1.on('finish', function() {
-    t2.play();
-  });
+    t1.on('finish', function() {
+      t2.play();
+    });
+  }
 
   // Dropdown button
   var dropdown = $('[data-dropdown]');
-  Array.from(dropdown).forEach(function(button) {
-    var contentId = button.getAttribute('data-dropdown');
-    button.addEventListener('click', function(e) {
-      var content = document.getElementById(contentId);
-      var thisBtnExpand = e.target.getAttribute('aria-expanded');
-      var contentHidden = content.getAttribute('aria-hidden');
-      button.setAttribute('aria-expanded', thisBtnExpand == 'true' ? 'false' : 'true');
-      content.setAttribute('aria-hidden', contentHidden == 'true' ? 'false' : 'true');
-      content.classList.toggle('drip-drop--expand');
-    });
-  });
 
-  yreveal({ reverse: false }, '.intro__icon');
+  if(dropdown.length) { // Homepage
+    Array.from(dropdown).forEach(function(button) {
+      var contentId = button.getAttribute('data-dropdown');
+      button.addEventListener('click', function(e) {
+        var content = document.getElementById(contentId);
+        var thisBtnExpand = e.target.getAttribute('aria-expanded');
+        var contentHidden = content.getAttribute('aria-hidden');
+        button.setAttribute('aria-expanded', thisBtnExpand == 'true' ? 'false' : 'true');
+        content.setAttribute('aria-hidden', contentHidden == 'true' ? 'false' : 'true');
+        content.classList.toggle('drip-drop--expand');
+      });
+    });
+
+    yreveal({ reverse: false }, '.intro__icon');
+
+  }else { // Doc pages
+
+    // Trigger menu on small viewports
+    var navMenu = $('.nav');
+    var page = $('.page');
+    console.log(navMenu[0], page[0], 'HeeHa');
+    navMenu[0].addEventListener('click', function() {
+      page[0].classList.toggle('page--navopen');
+    });
+
+  }
 });
--- _posts/2017-08-17-introduction.md
+++ _posts/2017-08-17-introduction.md
@@ -1,27 +1,44 @@
 ---
 layout: page
 title:  "Introduction"
-date:   2017-08-17 09:54:12
 permalink: /introduction/
 ---
 
 # Introduction
-You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
 
-To add new posts, simply add a file in the `_posts` directory that follows the convention `YYYY-MM-DD-name-of-post.ext` and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works.
+[![npm version](https://badge.fury.io/js/just-animate.svg)](https://badge.fury.io/js/just-animate) [![Build Status](https://travis-ci.org/just-animate/just-animate.svg?branch=master)](https://travis-ci.org/just-animate/just-animate)
 
-Jekyll also offers powerful support for code snippets:
+## Main Features
 
-{% highlight ruby %}
-def print_hi(name)
-  puts "Hi, #{name}"
-end
-print_hi('Tom')
-#=> prints 'Hi, Tom' to STDOUT.
-{% endhighlight %}
+* Animate a group of things as easily as a single thing
+* Staggering and delays
+* Chainable sequencing
+* Full timeline control \(reverse, pause, cancel, playbackRate, and seek\)
+* Alternate and repeat a complex series of animations (yo-yo)
+* Extendable timeline designed to make it simple to use multiple animation engines
+* Built-in tween engine that works with CSS Variables and regular objects alike
+* Plugin for Web Animation API
+* Hooks for custom interpolators such as Flubber
 
-Check out the [Jekyll docs][jekyll] for more info on how to get the most out of Jekyll. File all bugs/feature requests at [Jekyll’s GitHub repo][jekyll-gh]. If you have questions, you can ask them on [Jekyll’s dedicated Help repository][jekyll-help].
 
-[jekyll]:      http://jekyllrb.com
-[jekyll-gh]:   https://github.com/jekyll/jekyll
-[jekyll-help]: https://github.com/jekyll/jekyll-help
+> This project is powered by your stars [so ^ star it please](https://github.com/just-animate/just-animate/stargazers).
+
+## Quick Start Templates
+
+These CodePen templates can get you started immediately
+
+- https://codepen.io/notoriousb1t/pen/vJJEdo/
+
+## Need Help?
+
+Join the #just-animate channel on the [Animation at Work](https://damp-lake-50659.herokuapp.com/) slack, tweet [@notoriousb1t](https://twitter.com/notoriousb1t) on Twitter, or create an issue.
+
+## License
+
+Just Animate is licensed under the MIT license. \([http://opensource.org/licenses/MIT](http://opensource.org/licenses/MIT)\)
+
+## How can I contribute?
+
+* Make awesome things with Just Animate and send me the link!
+* Create an issue or PR on GitHub
+* Help with this documentation and code examples
